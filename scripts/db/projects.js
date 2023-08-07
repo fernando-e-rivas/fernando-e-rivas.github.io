a1b22971898fc75a3e4ecbc8b1de833df401fec3
Miguel <jemiguelrivas@gmail.com>
2023-08-07 03:16:37 -0700

    inabima project

--- scripts/db/projects.js
+++ scripts/db/projects.js
@@ -1,6 +1,7 @@
 export default [
   {
-    name: "Instituto de Bienestar Magisterial (INABIMA)",
+    name: "Instituto Nacional de Bienestar Magisterial",
+    shortName: "INABIMA",
     description: "Diseño, Coordinación y Supervisión General del Centro Odontológico, Apoyo Emocional, Salón de Eventos, Salón Consejo de Directores y Oficinas Administrativas del Instituto Nacional de Bienestar Magisterial (INABIMA), en Av. Pedro Henríquez Ureña No. 73 (Plaza Aurora).",
     cover: {
       url: "https://placehold.co/1920x1080",
@@ -22,6 +23,23 @@ export default [
             description: "Preview del Proyecto 3",
           },
         ],
+        cads: [],
+        content: [
+          "La parte fundamental de este proyecto, esta localizada en su 2do nivel, donde se ubican quince (15) unidades Odontologicas, incluyendo dos (2) para Cirugia, con el apoyo de Sala de Espera, Atencion al Publico, Baños Publico y de uso interno, Almacenes Administrativo y de Elementos Odontologicos, Area de Rayos X, Monitoreo de Equipos, Cocina y Comedor, incluyendo de manera prioritaria la accesibilidad sin fronteras, con la incorporacion de Ascensor, Baños para Discapacitados, entre otros.",
+          "Tambien se maneja un gran area en su azotea para manejo y colocacion de Equipos (Aire Acondicionado  Central tipo VRF, Unidades de Aire de Precision para Data Center, Unidades de Aire Comprimido, Bombas de Succion, Planta Electrica de Emergencia, Sistema de Tratamiento Osmosis y otros).",
+        ]
+      },
+      {
+        imgs: [
+          {
+            url: "https://placehold.co/1920x1080",
+            description: "Preview del Proyecto 1",
+          },
+          {
+            url: "https://placehold.co/1920x1080",
+            description: "Preview del Proyecto 2",
+          },
+        ],
         cads: [
           {
             url: "https://placehold.co/1920x1080",
@@ -29,10 +47,9 @@ export default [
           },
         ],
         content: [
-          "Centro Odontológico, Apoyo Emocional, Salón de Eventos, Salón Consejo de Directores.",
-          "La parte fundamiental de este proyecto, esta localizada en su 2do nivel, donde se ubican quince (15) unidades Odontologicas, incluyendo dos (2) para Cirugia, con el apoyo de Sala de Espera, Atencion al Publico, Baños Publico y de uso interno, Almacenes Administrativo y de Elementos Odontologicos, Area de Rayos X, Monitoreo de Equipos, Cocina y Comedor, incluyendo de manera prioritaria la accesibilidad sin fronteras, con la incorporacion de Ascensor, Baños para Discapacitados, entre otros. Tambien se maneja un gran area en su azotea para manejo y colocacion de Equipos (Aire Acondicionado  Central tipo VRF, Unidades de Aire de Precision para Data Center, Unidades de Aire Comprimido, Bombas de Succion, Planta Electrica de Emergencia, Sistema de Tratamiento Osmosis y otros) En su 3er nivel operan mayormente las Unidades de Atencion Emocional, asi como un gran Salon de Eventos, de Consejo de Directores, Espera, Baños Publico y de uso interno, Cocinilla, entre otros. En su 1er nivel, Oficinas Administrativas propias de la Institucion."
+          "En su 3er nivel operan mayormente las Unidades de Atencion Emocional, asi como un gran Salon de Eventos, de Consejo de Directores, Espera, Baños Publico y de uso interno, Cocinilla, entre otros. En su 1er nivel, Oficinas Administrativas propias de la Institucion.",
         ]
-      }
+      },
     ],
   }
 ]
