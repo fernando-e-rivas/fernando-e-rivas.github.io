b946e41bfb463929e09641b4dde16d47706bd976
Miguel <jemiguelrivas@gmail.com>
2023-08-07 02:50:57 -0700

    splitting app into sections

--- scripts/db/projects.js
+++ scripts/db/projects.js
@@ -3,7 +3,7 @@ export default [
     name: "Instituto de Bienestar Magisterial (INABIMA)",
     description: "Diseño, Coordinación y Supervisión General del Centro Odontológico, Apoyo Emocional, Salón de Eventos, Salón Consejo de Directores y Oficinas Administrativas del Instituto Nacional de Bienestar Magisterial (INABIMA), en Av. Pedro Henríquez Ureña No. 73 (Plaza Aurora).",
     cover: {
-      url: "https://placehold.co/1920/1080",
+      url: "https://placehold.co/1920x1080",
       description: "Preview del Proyecto",
     },
     sections: [
--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -1,12 +1,17 @@
 import "./modules/template7.min.js";
 import Perspective from "./components/perspective.js";
-import appData from "./db/app-data.js";
 
 window.customElements.define('mr-perspective', Perspective);
+
+import projects from "./db/projects.js";
+
+const data = {
+  projects,
+};
 
 const app = document.getElementById('app');
 const template = document.getElementById('template');
 const compiled = Template7(template.innerHTML).compile();
-const compiledRendered = compiled(appData);
+const compiledRendered = compiled(data);
 
 app.innerHTML = compiledRendered;
