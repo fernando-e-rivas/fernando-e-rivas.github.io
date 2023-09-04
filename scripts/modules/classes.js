5d267432ccbf8fd14eff33e9d4094d27b3fc939a
Miguel Rivas <jemiguelrivas@gmail.com>
2023-09-03 21:33:25 -0700

    adding original date input to datetime class

--- scripts/modules/classes.js
+++ scripts/modules/classes.js
@@ -23,19 +23,20 @@ export class Phone {
 
 export class DateTime {
   constructor(date = "Actual", format="year"){
+    this.original = date;
     if(date === "Actual"){
       this.display = date;
     } else if(format === 'year') {
       this.display = new Date(date).getFullYear();
     } else if(format === 'month') {
-    this.display = new Date(date).toLocaleDateString('es-do', { year:"numeric", month:"long"})
-  }
+      this.display = new Date(date).toLocaleDateString('es-do', { year:"numeric", month:"long"})
+    }
   }
   get iso() {
     if(this.display === "Actual"){
       return new Date();
     } else {
-      return new Date(this.display);
+      return new Date(this.original);
     }
   }
 };
