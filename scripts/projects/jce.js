a0cddda0f5724dc9424be0ae030c512a30edf982
Miguel Rivas <jemiguelrivas@gmail.com>
2023-09-03 23:35:12 -0700

    updating content for jce

--- scripts/projects/jce.js
+++ scripts/projects/jce.js
@@ -57,11 +57,74 @@ export default {
       imgs: [],
     },
     {
-      title: "Centro de Servicios y Legalizaciones (de caracter regional)",
+      title: "Centro de Servicios y Legalizaciones Santo Domingo Este (de caracter regional)",
       date: new DateTime("06/01/2008", "month"),
       content: [
         "Diseño y Ejecución Remodelación espacio para alojar dicho organismo, así como un centro de Cedulación, en un nivel, Oficinas administrativas, Recepción y espera Público, Cobros.",
         "Carretera Mella, Plaza Lumiere, Santo Domingo Este.",
+      ],
+      imgs: [],
+    },
+    {
+      title: "Centro de Servicios y Legalizaciones Santiago, 1ra y 3ra Oficialía del Estado Civil y Junta Electoral",
+      date: new DateTime("10/01/2007", "month"),
+      content: [
+        "Diseño y Supervisión Remodelación espacio para alojar dichos organismos, así como un centro de Cedulación, en dos niveles, Oficinas administrativas, Archivos, Baños público y de uso privado, Recepción y espera Público, Cobros.",
+        "Edificio del BNV (Sótano y 1er nivel), Calle Las Carreras, Santiago.",
+      ],
+      imgs: [],
+    },
+    {
+      title: "Centro de Servicios y Legalizaciones San Francisco de Macorís",
+      date: new DateTime("07/01/2008", "month"),
+      content: [
+        "Diseño y Supervisión Remodelación espacio para alojar dichos organismos, así como un centro de Cedulación, en el 1er nivel edificio de oficinas; Oficinas administrativas, Archivos, Baños público y de uso privado, Recepción y espera Público, Cobros.",
+        "Calle Duarte, San Francisco de Macoris.",
+      ],
+      imgs: [],
+    },
+    {
+      title: "2da Oficialía del Estado Civil Santiago",
+      date: new DateTime("07/01/2008", "month"),
+      content: [
+        "Diseño y Supervisión Remodelación espacio para alojar dicho organismo, así como un centro de Cedulación, en dos niveles, Oficinas administrativas, Archivos, Baños público y de uso privado, Recepción y espera Público, Cobros.",
+        "Calle Restauración, Santiago.",
+      ],
+      imgs: [],
+    },
+    {
+      title: "Centro de Servicios y Legalizaciones, Junta Electoral (Edificacion nueva) La Romana, 1ra Oficialía",
+      date: new DateTime("08/01/2009", "month"),
+      content: [
+        "Diseño y Supervisión espacio para alojar dichos organismos, así como un centro de Cedulación, en 2 niveles, Oficinas administrativas, Archivos, Baños público y de uso privado, Recepción y espera Público, Cobros.",
+        "Calle Rosa Duarte, La Romana.",
+      ],
+      imgs: [],
+    },
+    {
+      title: "1ra Oficialía del Estado Civil, Junta Electoral y Cedulación Los Llanos",
+      date: new DateTime("05/01/2010", "month"),
+      content: [
+        "Diseño y Supervisión Remodelación espacio que ocupaba antiguo Hospital, para alojar dichos organismos, así como un centro de Cedulación, en 1 nivel, Oficinas administrativas, Archivos, Baños público y de uso privado, Archivos, Recepción y espera Público, Cobros.",
+        "Calle Sánchez esquina San José, San Pedro de Macorís.",
+      ],
+      imgs: [],
+    },
+    {
+      title: "2da Oficialía del Estado Civil y Centro de Cedulación de La Romana",
+      date: new DateTime("08/01/2008", "month"),
+      content: [
+        "Diseño y Supervisión Remodelación espacio para alojar dichos organismos, Oficinas administrativas, Baños Publico y de uso privado, Archivos, Recepción y espera Público, Cobros.",
+        "La Romana.",
+      ],
+      imgs: [],
+    },
+    {
+      title: "Centro de Servicios y Legalizaciones Azua",
+      date: new DateTime("08/01/2008", "month"),
+      content: [
+        "Diseño y Ejecución Remodelación espacio para alojar dicho organismo, así como un centro de Cedulación, en 2do nivel edificio de oficinas; Oficinas administrativas, Baños público y de uso privado, Archivos, Recepción y espera Público, Cobros.",
+        "Av. Duarte esquina Armando Aybar, Azua.",
       ],
       imgs: [],
     },
